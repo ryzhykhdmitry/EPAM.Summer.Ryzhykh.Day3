140f98ccb475f95163ef95b83bb9be594324e210
Dmitry Ryzhykh <ryzhykh.dmitry@gmail.com>
2016-07-07 20:57:23 +0300

    Task 3 Finished(with NUnitTests), Edit MD file.

---
+++ BitAdder.NUnitTests/NUnitTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using NUnit.Framework;
+using BitAdder;
+
+namespace BitAdder.NUnitTests
+{
+    [TestFixture]
+    public class NUnitTest
+    {
+        [Test, TestCaseSource(typeof(MyDataClass), "TestCases")]
+        public void SummTwoNumbersParts_Summ255And853_Int863(int number1, int number2, int startPosition, int endPosition, int result)
+        {
+            Assert.That(Adder.insertIntoPosition(number1, number2, startPosition, endPosition), Is.EqualTo(result));
+        }
+
+        [Test]
+        public void SummTwoNumbersParts_Summ106And106_Int106()
+        {
+            Assert.That(Adder.insertIntoPosition(106, 106, 2, 5), Is.EqualTo(106));
+        }
+       
+        [Test, TestCaseSource(typeof(MyDataClass), "ExceptionTestCases")]
+        public void SummTwoNumbersParts_WrongPositionValues_ArgumentException(int number1, int number2, int startPosition, int endPosition)
+        {
+            Assert.That(() => { Adder.insertIntoPosition( number1, number2, startPosition, endPosition); } , Throws.TypeOf<ArgumentException>());
+        }
+           
+    }
+    public class MyDataClass
+    {
+        public static IEnumerable ExceptionTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(255, 523, 1, 0);
+                yield return new TestCaseData(213, 453, -4, -2);
+                yield return new TestCaseData(25, 52, 1, 35);
+                yield return new TestCaseData(255, 523, 37, 65);
+                yield return new TestCaseData(255, 523, -1, 3);
+
+            }
+        }
+
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(255, 853, 5, 9, 863);
+                yield return new TestCaseData(106, 106, 2, 5, 106);
+                yield return new TestCaseData(0, 7, 2, 2, 4);
+                yield return new TestCaseData(-7, 2, 1, 1, -5);
+
+            }
+        }
+    }
+}
---
+++ BitAdder/Adder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BitAdder
+{
+    /// <summary>
+    /// Allows to use some bit operations
+    /// </summary>
+    public class Adder
+    {
+        /// <summary>
+        /// Allows to insert one number into second number by bits
+        /// </summary>
+        /// <param name="number1">The number, in which we insert</param>
+        /// <param name="number2">The number, that we insert.</param>
+        /// <param name="startPosition">Start bit position </param>
+        /// <param name="endPosition">End bit position</param>
+        /// <returns>The number, that contains bits of two numbers</returns>
+        public static int insertIntoPosition(int number1, int number2, int startPosition, int endPosition)
+        {
+            if (startPosition > endPosition) throw new ArgumentException();
+            if (startPosition < 0 || endPosition < 0 || startPosition > 32 || endPosition > 32) throw new ArgumentException();
+
+            // converting the number to BitArray
+            BitArray a = new BitArray(new int[] { number1 });
+            BitArray b = new BitArray(new int[] { number2 });
+
+            for (int j = startPosition; j <= endPosition; j++)
+            {               
+                    a.Set(j, b.Get(j));            
+            }
+     
+            // converting BitArray to byte again
+            int[] array = new int[1];
+            a.CopyTo(array, 0);
+            return array[0];
+        }
+    }
+}
